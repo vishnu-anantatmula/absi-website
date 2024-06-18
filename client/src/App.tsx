a9367b5f1a8f05a4cafad6bc4b51daf9b9d1a1af
Malcolm Abbott <mjacoding@gmail.com>
2024-06-18 21:57:28 +0000

    Navbar, home page, browser router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,13 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './Components/NavBar';
+import { Home } from './Pages/Home';
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
-
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    readServerData();
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{serverData}</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
---
+++ client/src/Components/About.tsx
@@ -0,0 +1,21 @@
+export function About() {
+  return (
+    <>
+      <div>
+        From 100 year old boiler units to modern digital systems, Advanced
+        Building Services has over 25 years of experience of customizing and
+        providing excellent service ensuring we meet your needs. Our team is
+        equipped to provide full time building engineers to semi-annual
+        maintenance including HVAC, plumbing, electrical and even handyman
+        services. A proactive and consistent approach with the right maintenance
+        can add years to the life expectancy of your equipment, maximizing the
+        benefits of your investment and avoiding costly surprises.
+      </div>
+      <div>
+        We are just a phone call away to resolve any issue with our 24/7
+        emergency service. Our highly qualified and licensed team provides
+        exceptional quality for your essential building and HVAC needs.
+      </div>
+    </>
+  );
+}
