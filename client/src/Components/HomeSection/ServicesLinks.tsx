fcc3f7e5148c8c2afed71ba7d7f00c8f09cf9513
Malcolm Abbott <mjacoding@gmail.com>
2024-07-05 22:04:48 +0000

    Hashlink for Commercial HVAC Services section added to Link to commercial-hvac sercices section. Still waiting on the building-engineering section to be incorporated to be able to link to it through the nav bar.

--- client/src/Components/HomeSection/ServicesLinks.tsx
+++ client/src/Components/HomeSection/ServicesLinks.tsx
@@ -1,9 +1,12 @@
 import { HoverMenuItem } from './HoverMenuItem';
+import { HashLink } from 'react-router-hash-link';
 
 export function ServicesLinks() {
   return (
     <ul className="absolute cursor-pointer top-16 xl:top-20">
-      <HoverMenuItem name="Commercial HVAC" />
+      <HashLink to="#commercial-hvac">
+        <HoverMenuItem name="Commercial HVAC" />
+      </HashLink>
       <HoverMenuItem name="Building Engineering" />
     </ul>
   );
