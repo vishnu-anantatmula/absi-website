6e8403aa0c1d3e3efad2cdaba1aa810791822e64
Malcolm Abbott <mjacoding@gmail.com>
2024-07-16 23:07:20 +0000

    Both action sections completed. Dummy building engineering section placed between them.

--- src/Components/HomeSection/Consultations.tsx
+++ src/Components/HomeSection/Consultations.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 type Props = {
   description: string;
   buttonText: string;
@@ -7,9 +9,11 @@ export function Consultations({ description, buttonText }: Props) {
   return (
     <div className="grid place-items-center bg-blue-950 text-white py-10 gap-8 px-6">
       <p className="text-center text-lg">{description}</p>
-      <button className="font-semibold bg-white py-3 px-6 rounded-lg shadow-md basis-full text-blue-950 text-xl">
+      <Link
+        to="/contact"
+        className="font-semibold bg-white py-3 px-6 rounded-lg shadow-md basis-full text-blue-950 text-xl">
         {buttonText}
-      </button>
+      </Link>
     </div>
   );
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -52,6 +52,13 @@ export function Home({ isOpen, setIsOpen }: Props) {
         description={description[0]}
         buttonText="Book a Consultation"
       />
+      <div className="text-center py-16">
+        Dummy Building Engineering Section
+      </div>
+      <Consultations
+        description={description[1]}
+        buttonText="Book an Appointment"
+      />
     </div>
   );
 }
