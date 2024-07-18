adfe7ae7a114fc3133e984150e07c696fc552ab6
Malcolm Abbott <mjacoding@gmail.com>
2024-07-18 21:14:34 +0000

    Updated links to match the urls in figma. Minor tweaks elsewhere. Cleaned up code a little for FooterIcons.tsx

--- src/Components/HomeSection/Booking.tsx
+++ src/Components/HomeSection/Booking.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export function Booking() {
   return (
-    <div className="grid place-items-center gap-6 px-4 md:px-12 mt-4 mb-2">
+    <div className="grid place-items-center gap-8 px-4 md:px-12 mt-4 mb-2">
       <div className="xl:basis-full">
         <h2 className="font-semibold text-xl">Proud to be employee-owned.</h2>
       </div>
@@ -15,7 +15,7 @@ export function Booking() {
       <div className="xl:basis-full">
         <Link
           to="/contact"
-          className="font-semibold bg-blue-950 py-3 px-6 rounded-lg shadow-md basis-full text-white hover:ring-2 hover:ring-cyan-950">
+          className="font-semibold bg-blue-950 py-3 px-6 rounded-lg shadow-md basis-full text-white text-xl hover:ring-2 hover:ring-cyan-950">
           Book a Consultation
         </Link>
       </div>
