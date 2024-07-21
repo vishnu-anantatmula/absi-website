13f5f760d8441cd344ac024c812413b31eda8abf
Malcolm Abbott <mjacoding@gmail.com>
2024-07-21 18:00:41 +0000

    Added font-maven and corresponding breakpoints to Banner.tsx, About.tsx, and Booking.tsx

--- src/Components/HomeSection/Booking.tsx
+++ src/Components/HomeSection/Booking.tsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 export function Booking() {
   return (
-    <div className="grid place-items-center gap-6 px-4 md:px-12 mt-4 mb-2">
+    <div className="grid place-items-center gap-8 px-4 md:px-12 mt-4 mb-2 font-maven">
       <div className="xl:basis-full">
-        <h2 className="font-semibold text-xl">Proud to be employee-owned.</h2>
+        <h2 className="font-semibold text-2xl">Proud to be employee-owned.</h2>
       </div>
       <div className="xl:basis-full">
-        <h3 className="font-semibold text-center basis-full text-lg">
+        <h3 className="text-center basis-full text-xl">
           Our highly qualified and licensed team provides exceptional quality
           for your essential building and HVAC needs.
         </h3>
@@ -15,7 +15,7 @@ export function Booking() {
       <div className="xl:basis-full">
         <Link
           to="/contact"
-          className="font-semibold bg-blue-950 py-3 px-6 rounded-lg shadow-md basis-full text-white hover:ring-2 hover:ring-cyan-950">
+          className="font-semibold text-2xl bg-blue-950 py-3 px-6 rounded-lg shadow-md basis-full text-white hover:ring-2 hover:ring-cyan-950">
           Book a Consultation
         </Link>
       </div>
