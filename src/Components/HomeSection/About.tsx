d9f78f74b7bfe2fa23c915e099e6041f3c996614
Malcolm Abbott <mjacoding@gmail.com>
2024-07-21 18:44:19 +0000

    Styling: adjustments of line-height and spacing between letters in About.tsx && Banner.tsx && Booking.tsx

--- src/Components/HomeSection/About.tsx
+++ src/Components/HomeSection/About.tsx
@@ -1,13 +1,13 @@
 export function About() {
   return (
     <>
-      <div className="px-4 md:px-12 mt-4 font-maven text-lg xl:text-xl">
+      <div className="px-4 md:px-12 mt-4 font-maven text-xl xl:text-2xl leading-8">
         From 100 year old boiler units to modern digital systems, our team has
         over 25 years of experience of customizing and providing excellent
         service ensuring we meet your needs. We are just a phone call away to
         resolve any issue with our 24/7 emergency service.
       </div>
-      <div className="px-4 md:px-12 font-maven text-lg xl:text-xl">
+      <div className="px-4 md:px-12 font-maven text-xl xl:text-2xl leading-8">
         We are equipped to provide full time building engineers to semi-annual
         maintenance including HVAC, plumbing, electrical and even handyman
         services. A proactive and consistent approach with the right maintenance
--- src/Components/HomeSection/Booking.tsx
+++ src/Components/HomeSection/Booking.tsx
@@ -7,12 +7,12 @@ export function Booking() {
         <h2 className="font-semibold text-2xl">Proud to be employee-owned.</h2>
       </div>
       <div className="xl:basis-full">
-        <h3 className="text-center basis-full text-xl">
+        <h3 className="text-center basis-full text-xl font-semibold tracking-wide leading-relaxed">
           Our highly qualified and licensed team provides exceptional quality
           for your essential building and HVAC needs.
         </h3>
       </div>
-      <div className="xl:basis-full">
+      <div className="xl:basis-full mt-2">
         <Link
           to="/contact"
           className="font-semibold text-2xl bg-blue-950 py-3 px-6 rounded-lg shadow-md basis-full text-white hover:ring-2 hover:ring-cyan-950">
