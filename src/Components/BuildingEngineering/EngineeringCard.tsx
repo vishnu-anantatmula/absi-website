dfba8c04df57bc36aadf05f3e6b717d918ee21af
Devan Rivera <devanrivera98@gmail.com>
2024-09-05 22:09:04 +0000

    made webpage more ada compliant

--- src/Components/BuildingEngineering/EngineeringCard.tsx
+++ src/Components/BuildingEngineering/EngineeringCard.tsx
@@ -12,7 +12,7 @@ export default function EngineeringCard({ title, image, id }: Card) {
     <>
       <div key={id} className="pb-5 engineering-card-container">
         <div>
-          <img className="w-full" src={image} />
+          <img className="w-full" src={image} alt="" />
         </div>
         <div>
           <h1 className="py-2 text-lg font-bold font-maven">{title}</h1>
